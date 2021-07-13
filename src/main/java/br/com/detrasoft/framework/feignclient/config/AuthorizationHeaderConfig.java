76703fc5615bb9f141dc9f2a2f4173f25e3105ff
Detrasoft <46575134+Detrasoft@users.noreply.github.com>
2021-07-13 19:00:53 -0300

    Passando Locale entre requests dos microserviços

--- src/main/java/br/com/detrasoft/framework/feignclient/config/AuthorizationHeaderConfig.java
+++ src/main/java/br/com/detrasoft/framework/feignclient/config/AuthorizationHeaderConfig.java
@@ -2,6 +2,8 @@ package br.com.detrasoft.framework.feignclient.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
 
 import br.com.detrasoft.framework.security.context.UsuarioContext;
 import feign.RequestInterceptor;
@@ -15,7 +17,8 @@ public class AuthorizationHeaderConfig {
 		return new RequestInterceptor() {
 			@Override
 			public void apply(RequestTemplate template) {
-				template.header("Authorization", String.format("Bearer %s", UsuarioContext.getToken()));
+				template.header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", UsuarioContext.getToken()));
+				template.header(HttpHeaders.ACCEPT_LANGUAGE, LocaleContextHolder.getLocale().toString());
 			}
 		};
 	}
