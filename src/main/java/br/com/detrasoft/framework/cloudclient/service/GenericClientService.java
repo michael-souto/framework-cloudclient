d0aa68e6b71622ccaf99a088468cecab59f6f546
Michael Souto <michaelsouto@outlook.com>
2023-02-13 20:30:02 -0300

    Making improvements to the framework project

--- src/main/java/br/com/detrasoft/framework/cloudclient/service/GenericClientService.java
+++ src/main/java/br/com/detrasoft/framework/cloudclient/service/GenericClientService.java
@@ -1,36 +1,27 @@
 package br.com.detrasoft.framework.cloudclient.service;
 
-import java.util.List;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import br.com.detrasoft.framework.core.controller.Response;
- 
 public interface GenericClientService<T> {
 
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public ResponseEntity<Response<T>> create(@RequestBody T object);
-	
-	@RequestMapping(value = "/create-list", method = RequestMethod.POST)
-	public ResponseEntity<List<Response<T>>> createList(@RequestBody List<T> listEntity);
+	public ResponseEntity<T> insert(@RequestBody T object);
 
 	@RequestMapping(value = "/", method = RequestMethod.PUT)
-	public ResponseEntity<Response<T>> update(@RequestBody T object);
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Response<T>> findById(@PathVariable("id") Long id);
+	public ResponseEntity<T> update(@RequestBody T object);
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Response<T>> delete(@PathVariable("id") Long id);
+	public ResponseEntity<T> delete(@PathVariable("id") Long id);
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Response<List<T>>> findAll();
-	
-	@PostMapping(value = "/validate")
-	public ResponseEntity<Response<T>> validate(@RequestBody T object);
+	public ResponseEntity<T> findById(@PathVariable("id") Long id);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ResponseEntity<PagedModel<T>> findAll(Pageable pageable);
 }
