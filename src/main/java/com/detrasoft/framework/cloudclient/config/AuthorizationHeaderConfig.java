66a82abfbb6ea08be086e4a1f4002fd3dae5cb4b
Michael Souto <michaelsouto@outlook.com>
2023-05-10 13:47:37 -0300

    Fix token assignment for requests in other microservices

--- src/main/java/com/detrasoft/framework/cloudclient/config/AuthorizationHeaderConfig.java
+++ src/main/java/com/detrasoft/framework/cloudclient/config/AuthorizationHeaderConfig.java
@@ -1,31 +1,35 @@
 package com.detrasoft.framework.cloudclient.config;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.detrasoft.framework.core.context.GenericContext;
-import feign.RequestInterceptor;
-import feign.RequestTemplate;
+import javax.servlet.http.HttpServletRequest;
 
 @Configuration
 public class AuthorizationHeaderConfig {
 
-	@Bean
-	public RequestInterceptor bearerTokenRequestInterceptor() {
-		return new RequestInterceptor() {
-			@Override
-			public void apply(RequestTemplate template) {
-				template.header(HttpHeaders.ACCEPT_LANGUAGE, LocaleContextHolder.getLocale().toString());
-				if (RequestContextHolder.getRequestAttributes() != null && RequestContextHolder.getRequestAttributes() instanceof ServletRequestAttributes) {
-					HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
-					String authorization = request.getHeader("Authorization");
-					if (StringUtils.isNotBlank(authorization)) {
-						template.header("Authorization", new String[]{authorization});
-					}
-				}
-			}
-		};
-	}
+    @Bean
+    public RequestInterceptor bearerTokenRequestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                template.header(HttpHeaders.ACCEPT_LANGUAGE, LocaleContextHolder.getLocale().toString());
+                if (RequestContextHolder.getRequestAttributes() != null && RequestContextHolder.getRequestAttributes() instanceof ServletRequestAttributes) {
+                    HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+                    String authorization = request.getHeader("Authorization");
+                    if (StringUtils.isNotBlank(authorization)) {
+                        template.header("Authorization", new String[]{authorization});
+                    }
+                }
+            }
+        };
+    }
 }
+
