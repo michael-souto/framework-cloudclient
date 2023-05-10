c469d236bf2cd9d0626448b22ec3221d74fe7c7d
Michael Souto <michaelsouto@outlook.com>
2023-05-10 13:28:40 -0300

    Fix token assignment for requests in other microservices

--- src/main/java/com/detrasoft/framework/cloudclient/config/AuthorizationHeaderConfig.java
+++ src/main/java/com/detrasoft/framework/cloudclient/config/AuthorizationHeaderConfig.java
@@ -18,14 +18,12 @@ public class AuthorizationHeaderConfig {
 			@Override
 			public void apply(RequestTemplate template) {
 				template.header(HttpHeaders.ACCEPT_LANGUAGE, LocaleContextHolder.getLocale().toString());
-				if (GenericContext.getContexts("tenant") != null) {
-					template.header(
-							"tenant",
-							GenericContext.getContexts("tenant").toString());
-				} else if (GenericContext.getContexts("token") != null){
-					template.header(
-							HttpHeaders.AUTHORIZATION,
-							String.format("Bearer %s", GenericContext.getContexts("token").toString()));
+				if (RequestContextHolder.getRequestAttributes() != null && RequestContextHolder.getRequestAttributes() instanceof ServletRequestAttributes) {
+					HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+					String authorization = request.getHeader("Authorization");
+					if (StringUtils.isNotBlank(authorization)) {
+						template.header("Authorization", new String[]{authorization});
+					}
 				}
 			}
 		};
